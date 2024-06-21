116ac598cc11c27c220aa79eae4d4fae76fa64ae
SamuelDavidRuedaSuarezADSO <samueldavidruedasuarez3@gmail.com>
2024-06-21 16:54:11 -0500

    barra de navegacion mid, falta imagen deco fondo, barra navegacion

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
-var fechaActual = new Date();
-var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+let fechaActual = new Date();
+let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 document.getElementById('fecha').textContent = fechaActual.toLocaleDateString('es-ES', options);
+
+function toggleMenu() {
+  let sidebar = document.getElementById("sidebar");
+  if (sidebar.style.width === "200px") {
+    sidebar.style.width = "0";
+  } else {
+    sidebar.style.width = "200px";
+  }
+}
