ef10f94d262349652addd68422368bffb50f9f58
SamuelDavidRuedaSuarezADSO <samueldavidruedasuarez3@gmail.com>
2024-07-19 14:10:38 -0500

    Hecho: Login

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,13 +1,16 @@
 import { usuario } from "./modulo.js";
 
-let usuarios;
 let linkHome = "src/home-venta.html";
 
-const $boton = document.querySelector("#login");
 const $user = document.querySelector("#user");
 const $contra = document.querySelector("#contra");
 const $form = document.querySelector("#form");
-const $link = document.querySelector("#login > a")
+const $icon = document.querySelector(".login__icon");
+const $icon2 = document.querySelector("#candado");
+const $label = document.querySelector(".login__label");
+const $label2 = document.querySelector("#label");
+const $error = document.querySelector(".login__error");
+const $error2 = document.querySelector("#error");
 
 const validar = (event) => {
   event.preventDefault();
@@ -23,11 +26,25 @@ const validar = (event) => {
           if (user == x.nombre && passw == x.password) {
             location.href = linkHome;
           }
+          else{
+            $user.classList.add("error");
+            $contra.classList.add("error");
+            $icon.classList.add("error__icon");
+            $icon2.classList.add("error__icon");
+            $label.classList.add("error__icon");
+            $label2.classList.add("error__icon");
+            $user.value ="";
+            $contra.value ="";
+            $error.classList.remove("login__error");
+            $error2.classList.remove("login__error");
+            $error.classList.add("error__icon");
+            $error2.classList.add("error__icon");
+          }
         }); 
       })
       .catch(
         (error) => {
-          alert(error)
+          console.error(error);
         }
       )
   }
